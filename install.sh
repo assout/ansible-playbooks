0bbdc8d85ba58eaed51cd80f4937223a15ffc873
assout <assout@users.noreply.github.com>
2015-10-29 00:45:14 +0900

    fix to best practices

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://github.com/assout/ansible-playbooks.git
 cd ./ansible-playbooks
 
 sudo dnf install ansible -y
-ansible-playbook main.yml -Kvvv --skip-tags "fail"
+ansible-playbook site.yml -Kvvv --skip-tags "fail"
 
 git remote set-url origin git@github.com:assout/ansible-playbooks.git
 
