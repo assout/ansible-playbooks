2ce6daa7c3a798f69437ed583efc3ef2069b2a34
assout <assout@users.noreply.github.com>
2015-11-07 12:53:14 +0900

    fix to fedora 23

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ mkdir -p ~/Development
 cd ~/Development
 git clone https://github.com/assout/ansible-playbooks.git
 cd ./ansible-playbooks
+git remote set-url origin git@github.com:assout/ansible-playbooks.git
 
 sudo dnf install ansible -y
+sudo dnf install python-dnf -y
 ansible-playbook site.yml -Kvvv --skip-tags "fail"
-
-git remote set-url origin git@github.com:assout/ansible-playbooks.git
 
