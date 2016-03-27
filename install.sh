ad4334589d6cb39c56d8a2f4e8aa8499c9f90728
assout <assout@users.noreply.github.com>
2016-03-28 01:40:18 +0900

    Fix to ghq

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.ghq/github.com/assout/
-cd ~/.ghq/github.com/assout/
+mkdir -p ~/Development/github.com/assout/
+cd ~/Development/github.com/assout/
 git clone https://github.com/assout/ansible-playbooks.git
 cd ./ansible-playbooks
 git remote set-url origin git@github.com:assout/ansible-playbooks.git
